610d99336d9ae79d60f8e11a028012e15adff012
edgar <edgartomko88@gmail.com>
2022-09-28 17:31:25 +0300

    create new views, add 'REST_FRAMEWORK' settings

--- cards/serializers.py
+++ cards/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+
 from .models import Card
 
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ('user', 'title_native_language', 'translate_studied_language', 'category')
+        fields = '__all__'
--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
-from .models import Card
 
+from .models import Card
 from .serializers import CardSerializer
 
 
-class CardAPIView(generics.ListAPIView):
+class CardAPIList(generics.ListCreateAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class CardAPIUpdate(generics.UpdateAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class CardAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
