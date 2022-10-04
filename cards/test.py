36d7c8f2bfad91ea0ab3456913588ca2fc76878f
edgar <edgartomko88@gmail.com>
2022-10-04 16:47:58 +0300

    create CRUD tests for 'Card'

--- cards/test.py
+++ cards/test.py
@@ -11,30 +11,35 @@ from cards.serializers import CardSerializer
 
 class CardsTests(APITestCase):
     def setUp(self):
+        self.client = APIClient()
+
         user_test1 = User.objects.create_user(username='test1', password='1q2w3e4r5t')
         user_test1.save()
-
         user_test2 = User.objects.create_user(username='test2', password='123qwe')
         user_test2.save()
 
         self.user_test1_token = Token.objects.create(user=user_test1)
         self.user_test2_token = Token.objects.create(user=user_test2)
 
-        self.client = APIClient()
+        self.one_category = Category.objects.create(title="Category first user", user=user_test1)
+        self.two_category = Category.objects.create(title="Category second user", user=user_test2)
 
-        self.one_category = Category.objects.create(title="One_category", user=user_test1)
         self.one_card = Card.objects.create(
             user=user_test1,
             category=self.one_category,
             title_native_language='Test word',
-            translate_studied_language='Translate_test_word',
-        )
+            translate_studied_language='Translate_test_word')
+
+        self.two_card = Card.objects.create(
+            user=user_test2,
+            category=self.two_category,
+            title_native_language="Word for delete",
+            translate_studied_language="Translate word for delete")
 
         self.data = {
             "title_native_language": "Мир",
             "translate_studied_language": "World",
-            "category": self.one_category.id,
-        }
+            "category": self.one_category.id}
 
     def test_cards_info_invalid(self):
         response = self.client.get(reverse("user_cards"))
@@ -49,6 +54,14 @@ class CardsTests(APITestCase):
         response = self.client.post(reverse("user_cards"), self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_cards_create_another_group_invalid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.post(reverse("user_cards"), {"title_native_language": "Я",
+                                                            "translate_studied_language": "I",
+                                                            "category": self.two_category.id})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_cards_create_valid(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
         response = self.client.post(reverse("user_cards"), self.data)
@@ -58,6 +71,11 @@ class CardsTests(APITestCase):
         response = self.client.get(reverse("update_cards", kwargs={"pk": self.one_card.id}))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_cards_detail_info_invalid_pk(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.get(reverse("update_cards", kwargs={"pk": self.two_card.id}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_cards_detail_info_valid(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
         response = self.client.get(reverse("update_cards", kwargs={"pk": self.one_card.id}))
@@ -65,17 +83,37 @@ class CardsTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer_data, response.data)
 
-    def test_cards_update(self):
+    def test_cards_update_invalid(self):
+        response = self.client.put(reverse("update_cards", kwargs={"pk": self.one_card.id}), self.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cards_update_invalid_pk(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
-        test_data = {"title_native_language": "Test word change",
-                     "translate_studied_language": "Translate_test_word",
-                     "category": self.one_category.id}
+        response = self.client.put(reverse("update_cards", kwargs={"pk": self.two_card.id}), self.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        response = self.client.put(reverse("update_cards", kwargs={"pk": self.one_card.id}), test_data)
-
+    def test_cards_update_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.put(reverse("update_cards", kwargs={"pk": self.one_card.id}), self.data)
+        serializer_data = CardSerializer(Card.objects.get(pk=self.one_card.id)).data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["title_native_language"], "Test word change")
-        self.assertEqual(response.data["translate_studied_language"], "Translate_test_word")
+        self.assertEqual(serializer_data, response.data)
+
+    def test_cards_delete_invalid(self):
+        response = self.client.delete(reverse("delete_card", kwargs={"pk": self.one_card.id}))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_cards_delete_invalid_id(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.delete(reverse("delete_card", kwargs={"pk": self.two_card.id}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_cards_delete_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        result = Card.objects.all()
+        response = self.client.delete(reverse("delete_card", kwargs={"pk": self.one_card.id}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertNotEqual(result, Card.objects.all())
 
     def test_category_create_valid(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
